1e1cf88713bcc1acc0a819fc029ab0507680712e
Mehdi <bayarassou.mehdi@gmail.com>
2022-05-30 18:21:30 +0100

    Commit Module 05 Lab + solution

--- Module_05/Labs/Lab_01.cpp
+++ Module_05/Labs/Lab_01.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
- 
+#include <algorithm>
+#include <vector>
+
 using namespace std;
- 
+
+void printString(string name)
+{
+    cout << name << endl;
+}
+
+bool compareString(string s1, string s2)
+{
+    return s1 < s2;
+}
+
 int main()
 {
+    vector<string> dictAnimals {"cat", "dog", "horse", "fish", "bird", "turtle"};
 
+    //Print names of animals
+    for_each(dictAnimals.begin(), dictAnimals.end(), printString);
+
+    //Sort vector
+    sort(dictAnimals.begin(), dictAnimals.end(), compareString);
+
+    //Print names of animals
+    for_each(dictAnimals.begin(), dictAnimals.end(), printString);
+
+    cout << "Enter name of the animal you search for" << endl;
+    string animal;
+    cin >> animal;
+    //Find animal by name using find_if()
+
+
+    int nbLetters = 0;
+    //Sum of words lengths in nbLetters using for_each()
+
+    cout << "Sum of words lengths = "<< nbLetters << endl;
     return 0;
 }
--- Module_05/Solutions/Lab_solution_01.cpp
+++ Module_05/Solutions/Lab_solution_01.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
- 
+#include <algorithm>
+#include <vector>
+
 using namespace std;
- 
+
+void printString(string name)
+{
+    cout << name << endl;
+}
+
 int main()
 {
+    vector<string> dictAnimals {"cat", "dog", "horse", "fish", "bird", "turtle"};
+    auto lp = [](string name){cout << name << endl;};
 
+    //Print names of animals
+    for_each(dictAnimals.begin(), dictAnimals.end(), [](string name){cout << name << endl;});
+
+    //sort names of animals
+    sort(dictAnimals.begin(), dictAnimals.end(), [](string s1, string s2){return s1 < s2;});
+
+    //Print names of animals
+    for_each(dictAnimals.begin(), dictAnimals.end(), lp);
+
+    cout << "Enter name of the animal you search for" << endl;
+    string animal;
+    cin >> animal;
+    //Find animal by name using find_if()
+    auto result = find_if(dictAnimals.begin(), dictAnimals.end(), [animal](string s){return s == animal;}); 
+    (result != end(dictAnimals))
+        ? cout <<  animal << " is found" << '\n'
+        : cout <<  animal << " is not found" << '\n';
+
+    
+    int nbLetters = 0;
+    //Sum of words lengths in nbLetters using for_each()
+    for_each(dictAnimals.begin(), dictAnimals.end(), [&nbLetters](string s){nbLetters+=s.size();});
+    cout << "Sum of words lengths = "<< nbLetters << endl;
     return 0;
 }
